a4a12fbb23a2633b0b2c94c9c6083118c6174b63
Rashmi Prakash <rashmiprakash@Rashmis-MacBook-Pro.local>
2019-01-28 16:01:49 -0500

    tried debugging

--- looper/linkedlist.js
+++ looper/linkedlist.js
@@ -1,6 +1,6 @@
 class Node {
   constructor (value) {
-    this.value = value
+    this.value = value               
     this.previous = null
     this.next = null
   }
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,36 @@
+
 'use strict';
 
 // Complete this algo
 const minJumps = arr => {
+    let step = 0;
+    let pointer = 0;
+    let newArrObj = {};
+    //let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let key = i;
+        newArrObj[key] = arr[i];
 
+    }
+    console.log(newArrObj);
+    // console.log(newArrObj[pointer])
+    // console.log(newArrObj[pointer + 1])
+    while (pointer < arr.length) {
+        let endValue = newArrObj[pointer]
+        let max = 0;
+        for (let i = pointer + 1; i <= endValue; i++){
+            if (max < newArrObj[i ] ) {
+                max = newArrObj[i ]
+            }
+            console.log(`max : ${max}`)
+        }
+        console.log('uydsfgkuydfghdfku')
+        pointer = arr.indexOf(max);
+        console.log(pointer);
+        step++;
+    }
+    console.log(step)
+    return step;
 };
 
 module.exports = minJumps
